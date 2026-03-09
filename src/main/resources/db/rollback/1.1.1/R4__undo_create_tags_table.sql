cddf95aadc0a3b83b2d1de533980c4750cde15f1
Padmappiriyan <selvakumarpadmappiriyan@gmail.com>
2026-03-09 14:17:07 +0530

    New Version v1.1.0 released

--- src/main/resources/db/rollback/1.1.1/R4__undo_create_tags_table.sql
+++
@@ -1,6 +0,0 @@
--- =============================================================
--- Rollback   : R4__undo_create_tags_table.sql
--- Description: Drops the 'tags' table.
--- =============================================================
-
-DROP TABLE IF EXISTS tags;
